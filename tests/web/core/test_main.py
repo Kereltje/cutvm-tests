4a3cca32c5af10ebf6ff5e81f96820669eeacdc7
dimkroon <111366411+dimkroon@users.noreply.github.com>
2025-11-12 01:02:53 +0100

    [PROJECT] Short EPG in live plots

---
+++ tests/web/core/test_main.py
@@ -0,0 +1,25 @@
+
+from unittest import TestCase
+from unittest.mock import patch
+
+import resources.lib.kodi_utils
+
+patches = []
+
+
+# def setUpModule():
+#     patches.append(patch('resources.lib.kodi_utils.get_proxy', return_value=None))
+#     for p in patches:
+#         p.apply()
+
+resources.lib.kodi_utils.get_proxy = lambda: None
+
+from resources.lib import main
+
+main.xbmc.getRegion = lambda x: '%H:%M' if x == 'time' else ''
+
+
+class LiveWithGuide(TestCase):
+    def test_live_menu_with_guide(self):
+        results = main.tv_guide_menu.test('uk_live')
+        pass
